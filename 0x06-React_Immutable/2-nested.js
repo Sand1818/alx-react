832186171a055462a53f6284ee182c6c581f868d
Sand1818 <sandiso.jele@gmail.com>
2024-09-26 18:15:55 +0200

    2-nested fix

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,6 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const map = Map(object);
+  const map = fromJS(object);
   return map.getIn(array, undefined);
 }
